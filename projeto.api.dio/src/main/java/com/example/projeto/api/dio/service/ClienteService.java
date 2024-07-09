68f06621c20963bcb88bf3b1face5382dbc1623c
Eduardo Abrahao <112214564+Abrahao02@users.noreply.github.com>
2024-07-09 07:54:06 -0300

    Desafio Dio Spring API cep
    Design Patterns com Java: Dos Clássicos (GoF) ao Spring Framework

---
+++ projeto.api.dio/src/main/java/com/example/projeto/api/dio/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.example.projeto.api.dio.service;
+
+import com.example.projeto.api.dio.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
